b0f80d27d06ab29daeee41afa1f26b6905f9a9a7
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-01-17 16:01:40 +0800

    Fixed all operations. Now just needs dynamic programming.

--- main.rb
+++ main.rb
@@ -1,15 +1,52 @@
 # This is a ruby program in handling string operations.
+puts ("CSADPRG MP String Operations w/ Distance")
+
+@pointer = 0
+operStr = ""
+
+# Operation Methods
+def copy(x)
+    x
+end
+
+def insert(str,x)
+    # Creates a substring from the pointer index to end.
+    operStr = str
+    subStr = operStr.slice!(@pointer..-1)
+    operStr[@pointer] = x
+    operStr+subStr
+end
+
+def delete(str)
+    operStr = str
+    subStr = operStr.slice!(@pointer..-1)
+    # Pointer moved back by one so it stays in the same index.
+    @pointer -= 1
+    operStr+subStr.slice!(1..-1)
+end
+
+def replace(str, x)
+    operStr = str
+    operStr[@pointer] = x
+    operStr
+end
 
 
 string1 = "intention"
 string2 = "execution"
-operStr = ""
+
 
 distance = 0
 
-puts "Edit Distance: "
-distance = (Integer)gets
+print "Edit Distance: "
+parseInt = gets.chomp
+distance = Integer(parseInt)
+operStr = copy(string1)
 
-(0..distance).each do |i|
-    puts "Operation #{i}"
+# This performs the operations based on the size of distance.
+(0...distance).each do |i|
+    # puts "Operation #{i}"
+    operStr = replace(operStr,'a')
+    puts "Now: #{operStr}"
+    @pointer += 1
 end
