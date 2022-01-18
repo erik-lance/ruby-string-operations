03d0ae5c456054c27635d2cb66fef4a1a5139e26
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-01-18 16:02:37 +0800

    Added operations checker
    Now looks through the array diagonally so that printing can be done properly.
    
    Proper if statements to be added soon.

--- main.rb
+++ main.rb
@@ -7,24 +7,7 @@
 # This is a ruby program in handling string operations.
 puts ("CSADPRG MP String Operations w/ Distance")
 
-operStr = ""
 
-# Operation Methods
-def copy(x)
-    x
-end
-
-
-def editStringDist(str1, str2, x,y)
-    if    x == 0 then y
-    elsif y == 0 then x
-    elsif (str1[x-1] == str2[y-1]) then editStringDist(str1,str2,x,y)
-    else
-        value = 1 + [editStringDist(str1,str2,x,y-1),
-                    editStringDist(str1,str2,x-1,y),
-                    editStringDist(str1,str2,x-1,y-1)].min
-    end
-end
 
 def minEditDist(str1,str2, x, y, matrix)
     # matrix.each { |x|
@@ -42,18 +25,21 @@ def minEditDist(str1,str2, x, y, matrix)
         val2 = 0
         val3 = 0
 
+        # Insert
         if    matrix[x-1][y] != -1 then val1 = matrix[x-1][y]
         else  val1 = minEditDist(str1,str2,x-1,y, matrix)
         end
 
+        # Remove
         if    matrix[x][y-1] != -1 then val2 = matrix[x][y-1]
         else  val2 = minEditDist(str1,str2,x,y-1, matrix)
         end
 
+        # Replace
         if    matrix[x-1][y-1] != -1 then val3 = matrix[x-1][y-1]
         else  val3 = minEditDist(str1,str2,x-1,y-1, matrix)
         end
-        
+        @globalMatrix = matrix
         return matrix[x][y] = 1 + [val1,val2,val3].min
     end
 end
@@ -63,19 +49,16 @@ string2 = "execution"
 
 distance = 0
 strMatrix = Array.new(string1.length+1) { Array.new(string2.length+1) {-1} }
+@globalMatrix = Array.new(string1.length+1) { Array.new(string2.length+1) {-1} }
 
+puts "Input string 1 = #{string1}"
+puts "Input string 2 = #{string2}"
 
 print "Edit Distance: "
 print minEditDist(string1, string2, string1.length, string2.length, strMatrix)
-# print editStringDist(string1,string2,string1.length,string2.length)
-# parseInt = gets.chomp
-# distance = Integer(parseInt)
-operStr = copy(string1)
 
-# # This performs the operations based on the size of distance.
-# (0...distance).each do |i|
-#     # puts "Operation #{i}"
-#     operStr = replace(operStr,'a')
-#     puts "Now: #{operStr}"
-#     @pointer += 1
-# end
+puts "\nOperations: "
+
+(0...string1.length+1).each do |i|
+    puts "Operation "+@globalMatrix[i][i].to_s
+end
