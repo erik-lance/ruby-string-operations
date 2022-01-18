3513953fa2ec4c29e68d386344c35ed8fa8884c8
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-01-18 15:47:37 +0800

    Cleaned Up, fixed array bug
    Realized some parts of the array were wrong. Forgot about lengths and such and adjusted them accordingly.
    
    Cleaned up previous code on insert/delete/replace and instead using this more dynamic and safe method.

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
+# *********************
+# Name: Erik Lance L. Tiongquico
+# Language: Ruby
+# Paradigm: Object-Oriented
+# *********************
+
 # This is a ruby program in handling string operations.
 puts ("CSADPRG MP String Operations w/ Distance")
 
-@pointer = 0
 operStr = ""
 
 # Operation Methods
@@ -9,27 +14,6 @@ def copy(x)
     x
 end
 
-def insert(str,x)
-    # Creates a substring from the pointer index to end.
-    operStr = str
-    subStr = operStr.slice!(@pointer..-1)
-    operStr[@pointer] = x
-    operStr+subStr
-end
-
-def delete(str)
-    operStr = str
-    subStr = operStr.slice!(@pointer..-1)
-    # Pointer moved back by one so it stays in the same index.
-    @pointer -= 1
-    operStr+subStr.slice!(1..-1)
-end
-
-def replace(str, x)
-    operStr = str
-    operStr[@pointer] = x
-    operStr
-end
 
 def editStringDist(str1, str2, x,y)
     if    x == 0 then y
@@ -42,13 +26,48 @@ def editStringDist(str1, str2, x,y)
     end
 end
 
+def minEditDist(str1,str2, x, y, matrix)
+    # matrix.each { |x|
+    #     puts x.join(" ")
+    #    }
+    if    x == 0 then return y
+    elsif y == 0 then return x
+    elsif (matrix[x][y] != -1) then return matrix[x][y]
+    elsif str1[x-1] == str2[y-1] then
+        if(matrix[x-1][y-1] == -1) then return matrix[x][y] = minEditDist(str1,str2,x-1,y-1,matrix)
+        else return matrix[x][y] = matrix[x-1][y-1]
+        end
+    else
+        val1 = 0
+        val2 = 0
+        val3 = 0
+
+        if    matrix[x-1][y] != -1 then val1 = matrix[x-1][y]
+        else  val1 = minEditDist(str1,str2,x-1,y, matrix)
+        end
+
+        if    matrix[x][y-1] != -1 then val2 = matrix[x][y-1]
+        else  val2 = minEditDist(str1,str2,x,y-1, matrix)
+        end
+
+        if    matrix[x-1][y-1] != -1 then val3 = matrix[x-1][y-1]
+        else  val3 = minEditDist(str1,str2,x-1,y-1, matrix)
+        end
+        
+        return matrix[x][y] = 1 + [val1,val2,val3].min
+    end
+end
+
 string1 = "intention"
 string2 = "execution"
 
-
 distance = 0
+strMatrix = Array.new(string1.length+1) { Array.new(string2.length+1) {-1} }
+
 
 print "Edit Distance: "
+print minEditDist(string1, string2, string1.length, string2.length, strMatrix)
+# print editStringDist(string1,string2,string1.length,string2.length)
 # parseInt = gets.chomp
 # distance = Integer(parseInt)
 operStr = copy(string1)
