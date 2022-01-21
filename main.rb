d9f5f17255a23bf44a7d6cec596a5d22d97df1e6
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-01-21 20:58:33 +0800

    Cleaned up functions
    Project is at its final stage now. Memoization works as intended.
    
    Note: In printing the operations, the matrix at 0,0 is temporarily set to 0 to accommodate printing calculations, especially since all are set to -1 horizontally and vertically from zero.

--- main.rb
+++ main.rb
@@ -7,12 +7,7 @@
 # This is a ruby program in handling string operations.
 puts ("CSADPRG MP String Operations w/ Distance")
 
-
-
 def minEditDist(str1,str2, x, y, matrix)
-    # matrix.each { |x|
-    #     puts x.join(" ")
-    #    }
     if x == 0 then return y end
     if y == 0 then return x end
 
@@ -54,43 +49,6 @@ def minEditDist(str1,str2, x, y, matrix)
 end
 
 # Prints the edits to reach the goal string from [x][y]
-def adjprintEdits(matrix, str1, str2)
-    # Grabs the length of horizontal and vertical in matrix
-    x = 0
-    y = 0
-    loop = true
-    printList = Array.new
-    while(loop) 
-        if (x==str1.length-1|| y==str2.length-1) then 
-            loop = false
-            break; 
-        end
-        if (str1[x+1]==str2[y+1])
-            x = x+1
-            y = y+1
-        elsif (matrix[x][y] == matrix[x-1][y]+1)
-            printList.push("Delete "+str1[x-1])
-            x = x-1
-        elsif (matrix[x][y] == matrix[x][y-1]+1)
-            printList.push("Insert "+str2[y-1])
-            y= y-1
-        elsif (matrix[x][y] == matrix[x-1][y-1]+1)
-            printList.push("Replace "+str1[x-1]+" with "+str2[y-1])
-            x = x-1
-            y = y-1
-        else
-            puts "ERROR "+x.to_s+" and "+y.to_s
-            loop = false
-        end
-    end
-
-    # This prints in reverse order to show the proper logic
-    (-printList.length()...1).each do |x|
-        puts printList[x.abs]
-    end
-end
-
-# Prints the edits to reach the goal string from [x][y]
 def printEdits(matrix, str1, str2)
     # For print calculation purposes, the index 0,0 must be 0
     # instead of -1 due to dynamic programming and memoization.
@@ -119,15 +77,8 @@ def printEdits(matrix, str1, str2)
             printList.push("Replace "+str1[x-1]+" with "+str2[y-1])
             x = x-1
             y = y-1
-        elsif (matrix[x][y] == matrix[x-1][y-1]+2 && x==1)
-            printList.push("Replace "+str1[x-1]+" with "+str2[y-1])
-            x = x-1
-            y = y-1
         else
-            puts "ERROR "+x.to_s+" and "+y.to_s+": "+matrix[x][y].to_s
-            puts "ERROR "+(x-1).to_s+" and "+(y-1).to_s+": "+matrix[x-1][y-1].to_s
-            puts "str1 "+str1[x]+" str2 "+str2[x]
-            puts "str1 "+str1[x-1]+" str2 "+str2[x-1]
+            puts "ERROR"
             loop = false
         end
     end
@@ -143,7 +94,7 @@ string2 = "execution"
 
 # This simply creates a matrix of value -1 (which is the base value)
 strMatrix = Array.new(string1.length+1) { Array.new(string2.length+1) {-1} }
-@globalMatrix = Array.new(string1.length+1) { Array.new(string2.length+1) {-1} }
+@globalMatrix = strMatrix
 
 puts "Input string 1 = #{string1}"
 puts "Input string 2 = #{string2}"
@@ -151,10 +102,10 @@ puts "Input string 2 = #{string2}"
 print "Edit Distance: "
 print minEditDist(string1, string2, string1.length(), string2.length(), strMatrix)
 
-puts "\nOperations: "
+print "\n\nOperations: "
 strMatrix = @globalMatrix
 printEdits(strMatrix, string1, string2)
 
-width = @globalMatrix.flatten.max.to_s.size+2
-  #=> 4
-puts strMatrix.map { |a| a.map { |i| i.to_s.rjust(width) }.join }
+# Feel free to uncomment to debug how the matrix looks like.
+# width = @globalMatrix.flatten.max.to_s.size+2
+# puts strMatrix.map { |a| a.map { |i| i.to_s.rjust(width) }.join }
